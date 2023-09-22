0286db118e912c916a6570180831eebeb085ac82
Ubuntu <ubuntu@ip-172-31-2-201.us-east-2.compute.internal>
2023-09-22 05:46:53 +0000

    Created api for standard diffusion

--- sdxl_script.py
+++ sdxl_script.py
@@ -4,19 +4,10 @@ from diffusers import StableDiffusionControlNetPipeline, ControlNetModel, UniPCM
 import torch
 import numpy as np
 from diffusers.utils import load_image
-import bitsandbytes as bnb
 
-image = load_image("https://huggingface.co/datasets/hf-internal-testing/diffusers-images/resolve/main/sd_controlnet/hf-logo.png")
-negative_prompt = 'low quality, bad quality, sketches'
-image = np.array(image)
+from fastapi import FastAPI, UploadFile, Form
+from typing import Annotated
 
-low_threshold = 100
-high_threshold = 200
-
-image = cv2.Canny(image, low_threshold, high_threshold)
-image = image[:, :, None]
-image = np.concatenate([image, image, image], axis=2)
-image = Image.fromarray(image)
 
 controlnet = ControlNetModel.from_pretrained(
     "lllyasviel/sd-controlnet-canny", torch_dtype=torch.float16
@@ -35,6 +26,30 @@ pipe.enable_xformers_memory_efficient_attention()
 
 pipe.enable_model_cpu_offload()
 
-image = pipe("aerial view, a futuristic research complex in a bright foggy jungle, hard lighting",image, num_inference_steps=20, negative_prompt=negative_prompt).images[0]
 
-image.save('bird_canny_out.png')
+app = FastAPI()
+
+@app.get("/process_image/")
+async def process_image():
+
+    image = load_image("https://huggingface.co/datasets/hf-internal-testing/diffusers-images/resolve/main/sd_controlnet/hf-logo.png")
+    
+
+    negative_prompt = 'low quality, bad quality, sketches'
+    image = np.array(image)
+
+    low_threshold = 100
+    high_threshold = 200
+
+    image = cv2.Canny(image, low_threshold, high_threshold)
+    image = image[:, :, None]
+    image = np.concatenate([image, image, image], axis=2)
+    image = Image.fromarray(image)
+    image = pipe("aerial view, a futuristic research complex in a bright foggy jungle, hard lighting",image, num_inference_steps=20, negative_prompt=negative_prompt).images[0]
+    #image.save('bird_canny_out.png')
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
