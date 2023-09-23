645b8a271721b21af91cba00cadd233e06b6bb8d
Ubuntu <ubuntu@ip-172-31-2-201.us-east-2.compute.internal>
2023-09-23 01:15:35 +0000

    restructured api

---
+++ config.py
@@ -0,0 +1,33 @@
+from diffusers import StableDiffusionControlNetPipeline, ControlNetModel, UniPCMultistepScheduler
+import torch
+
+controlnet = ControlNetModel.from_pretrained(
+    "lllyasviel/sd-controlnet-canny", torch_dtype=torch.float16
+)
+
+pipe = StableDiffusionControlNetPipeline.from_pretrained(
+    "runwayml/stable-diffusion-v1-5", controlnet=controlnet, safety_checker=None, torch_dtype=torch.float16
+)
+
+pipe.scheduler = UniPCMultistepScheduler.from_config(pipe.scheduler.config)
+
+# Remove if you do not have xformers installed
+# see https://huggingface.co/docs/diffusers/v0.13.0/en/optimization/xformers#installing-xformers
+# for installation instructions
+pipe.enable_xformers_memory_efficient_attention()
+
+pipe.enable_model_cpu_offload()
+
+
+control_net_scribble = ControlNetModel.from_pretrained(
+    "lllyasviel/sd-controlnet-scribble", torch_dtype=torch.float16
+)
+
+control_net_pipe = StableDiffusionControlNetPipeline.from_pretrained(
+    "runwayml/stable-diffusion-v1-5", controlnet=control_net_scribble, safety_checker=None, torch_dtype=torch.float16
+)
+
+control_net_pipe.scheduler = UniPCMultistepScheduler.from_config(control_net_pipe.scheduler.config)
+control_net_pipe.enable_xformers_memory_efficient_attention()
+
+control_net_pipe.enable_model_cpu_offload()
---
+++ server.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI, UploadFile, Form
+from typing import Annotated
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import StreamingResponse
+import requests
+
+from .app.endpoints import sketch
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# app.include_router(sketch.router)
+
+app.include_router(sketch.router)
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
